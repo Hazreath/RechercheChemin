93bebecc993f69cc5f4527410d3d695f2546ac0f
UnChauve31 <48658578+UnChauve31@users.noreply.github.com>
2019-03-31 19:32:36 +0200

    Add files via upload

---
+++ source/Chemin.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Chemin {
+	private List<int[]> contenu;
+
+	public List<int[]> getContenu() {
+		return contenu;
+	}
+
+	public void setContenu(List<int[]> contenu) {
+		this.contenu = contenu;
+	}
+	
+	public boolean tester(Resultat r) {
+		return true;
+	}
+}
---
+++ source/IResultat.java
@@ -0,0 +1,8 @@
+
+public interface IResultat {
+	static final int nbIteration = 0;
+	static final double timeExec = 0.0;
+
+	public void afficher();
+
+}
